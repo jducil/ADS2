7621160acbce8404e1468309bfb52efc81b904f6
jducil <127637176+jducil@users.noreply.github.com>
2023-12-18 22:57:46 +0800

    Update stapp.py

--- stapp.py
+++ stapp.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import tensorflow as tf
 import pandas as pd
+import numpy as np
+
 
 # Load the model
 @st.cache(allow_output_mutation=True)
@@ -16,8 +18,23 @@ st.write("# Bike Sharing Demand Prediction")
 # CSV File Uploader
 uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
 
-# Define a function to preprocess the data
-
 def make_prediction(data, model):
     prediction = model.predict(data)
-return prediction
+    return prediction
+
+if uploaded_file is not None:
+    try:
+        # Read and preprocess the data
+        input_df = pd.read_csv(uploaded_file)
+        preprocessed_data = preprocess_data(input_df)
+
+        if st.sidebar.button('Predict Demand'):
+            prediction = make_prediction(preprocessed_data, model)
+            # Display the prediction
+            st.subheader("Bike Sharing Demand Prediction")
+            st.write(f"Predicted Bike Demand: {prediction[0][0]:.2f} bikes")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
+
+else:
+    st.sidebar.write("Please upload a CSV file to make predictions.")
